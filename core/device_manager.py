462ab2d774a9bc94520fc4a768f10018c4589fca
Matt Copperwaite <mattcopp@gmail.com>
2012-03-26 20:43:56 +0100

    Edd starting point

---
+++ core/device_manager.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+ This file is part of PROJECT NAME.
+
+ PROJECT NAME is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ PROJECT NAME is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with PROJECT NAME.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from stat import S_ISBLK as is_block_device
+
+class UnixDeviceList(object):
+    def __init__(self, find=True):
+        self.__devices = []
+        if find:
+            self.find_devices()
+        
+    def find_devices(self):
+        pass
+        
+    def get_devices(self):
+        pass
+
+class UnixDevice(file):
+    def __init__(self, device_path, mode="wb"):
+        if is_block_device(device_path):
+            file.__init__(self, device_path, mode)
+        else:
+            raise IOError("The device path is not a block device.")
+
+if __name__ == "__main__":
+    pass
---
+++ core/writer.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+ This file is part of PROJECT NAME.
+
+ PROJECT NAME is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ PROJECT NAME is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with PROJECT NAME.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from os import fstat
+from stat import S_ISBLK as is_block_device
+from time import time as now
+
+class Writer(object):
+    def __init__(self, source_file, destination_device, block_size=1024):
+        self._source_file = source_file
+        self._destination_device = destination_device
+        self.__block_size = block_size
+        
+        self.file_size = os.fstat(self.__source_file.fileno()).st_size
+        self.current_transfered = 0
+        self.bps = 0
+        self.last_diff = 0
+        
+    def get_errors(self):
+        errors = []
+        if not hasattr(self._source_file, 'read'):
+            errors.append("Source file does not have a read attribute.")
+        if not hasattr(self._destination_device, 'write'):
+            errors.append("Destination device does not have a write attribute.")
+        return errors
+        
+    def has_errors(self):
+        return bool(self.get_errors())
+        
+    def start(self):
+        while True:
+            buff = self._source_file.read(self.__block_size)
+            if not buff:
+                break
+            self._destination_device.write(buff)
+            buffer_length = len(buff)
+            self.__update_metrics(buffer_length)
+            
+    def __update_metrics(self, buffer_length):
+        diff = self.__get_time_diff()
+        if diff:
+            self.total_downloaded += buffer_length
+            
+            speed_multiplier = 1.0 / diff
+            bits_ps = downloaded_length * speed_multiplier
+            self.bps = bits_ps / float(8 * 1024)
+        
+    def __get_diff(self):
+        now = now()
+        time_diff = 0
+        if self.last_time:
+            time_diff = now - last_time
+        self.last_time = now
+        return time_diff
+
+if __name__ == "__main__":
+    pass
