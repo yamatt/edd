988ee9c13d1d1493877fb698b427f2f5131193fe
Matt Copperwaite <mattcopp@gmail.com>
2012-03-28 20:01:52 +0100

    Added english language translations

---
+++ ui/languages/edd.en_gb.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_GB">
+<context>
+    <name>main_window</name>
+    <message>
+        <location filename="edd.ui" line="14"/>
+        <source>Edd</source>
+        <translation>Edd</translation>
+    </message>
+    <message>
+        <location filename="edd.ui" line="27"/>
+        <source>Select the device you would like to write to:</source>
+        <translation>Select the device you would like to write to:</translation>
+    </message>
+    <message>
+        <location filename="edd.ui" line="53"/>
+        <source>Reload</source>
+        <translation>Reload</translation>
+    </message>
+    <message>
+        <location filename="edd.ui" line="72"/>
+        <source>Select the file you want to write to the device:</source>
+        <translation>Select the file you want to write to the device:</translation>
+    </message>
+    <message>
+        <location filename="edd.ui" line="98"/>
+        <source>Browse...</source>
+        <translation>Browse...</translation>
+    </message>
+    <message>
+        <location filename="edd.ui" line="111"/>
+        <source>Close</source>
+        <translation>Close</translation>
+    </message>
+    <message>
+        <location filename="edd.ui" line="124"/>
+        <source>Start</source>
+        <translation>Start</translation>
+    </message>
+</context>
+</TS>
