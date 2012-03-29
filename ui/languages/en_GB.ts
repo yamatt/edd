a794d1e60efd865aef07f97cdd5cf519727bb433
Matt Copperwaite <mattcopp@gmail.com>
2012-03-29 21:15:18 +0100

    Combined translation files in to one file

---
+++ ui/languages/en_GB.ts
@@ -0,0 +1,65 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_GB">
+<context>
+    <name>main_window</name>
+    <message>
+        <location filename="../edd.py" line="21"/>
+        <source>Edd</source>
+        <translation>Edd</translation>
+    </message>
+    <message>
+        <location filename="../edd.py" line="26"/>
+        <source>Select the device you would like to write to:</source>
+        <translation>Select the device you would like to write to:</translation>
+    </message>
+    <message>
+        <location filename="../edd.py" line="33"/>
+        <source>Reload</source>
+        <translation>Reload</translation>
+    </message>
+    <message>
+        <location filename="../edd.py" line="39"/>
+        <source>Select the file you want to write to the device:</source>
+        <translation>Select the file you want to write to the device:</translation>
+    </message>
+    <message>
+        <location filename="../edd.py" line="46"/>
+        <source>Browse...</source>
+        <translation>Browse...</translation>
+    </message>
+    <message>
+        <location filename="../edd.py" line="50"/>
+        <source>Close</source>
+        <translation>Close</translation>
+    </message>
+    <message>
+        <location filename="../edd.py" line="54"/>
+        <source>Start</source>
+        <translation>Start</translation>
+    </message>
+</context>
+<context>
+    <name>progress</name>
+    <message>
+        <location filename="../edd-progress.py" line="21"/>
+        <source>Progress...</source>
+        <translation>Progress...</translation>
+    </message>
+    <message>
+        <location filename="../edd-progress.py" line="24"/>
+        <source>Progress:</source>
+        <translation>Progress:</translation>
+    </message>
+    <message>
+        <location filename="../edd-progress.py" line="35"/>
+        <source>&amp;Cancel</source>
+        <translation>&amp;Cancel</translation>
+    </message>
+    <message>
+        <location filename="../edd-progress.py" line="39"/>
+        <source>&amp;OK</source>
+        <translation>&amp;OK</translation>
+    </message>
+</context>
+</TS>
